b1267de2dd29868bb611bff3abff8ff5216258d5
JomarR <JomarR@Jomar>
2016-02-05 23:30:04 -0200

    Test Commit

---
+++ Compiladores/src/Main/Main.java
@@ -0,0 +1,45 @@
+package Main;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
+import compilador.lexer.Lexer;
+import compilador.lexer.LexerException;
+import compilador.node.*;
+  
+import java.io.* ; 
+  
+public class Main { 
+	public void AnaliseLexica(String arquivo, Lexer lexer)
+			throws IOException {
+		
+
+		Token token = null;
+		boolean flag = false;
+		do {
+			try {
+				while (!((token = lexer.next()) instanceof EOF)) {
+					if (!(token instanceof TBlank)) {
+						System.out.print(token.getClass().getSimpleName());
+					} else {
+						System.out.print(token.getText());
+					}
+				}
+				flag = true;
+			} catch(LexerException e) {
+				System.out.print(e.getMessage().substring(0, e.getMessage().length() - 1) + " ");
+			}
+		} while(!(flag));
+	
+	}
+	public static void main (String[] args) throws LexerException, IOException{
+		//String arquivo = "/src/tester.sa";
+		Lexer lexer = new Lexer (new PushbackReader( 
+	               new FileReader(args[0]), 1024)); 
+		Main main = new Main();
+		
+		main.AnaliseLexica(args[0], lexer);
+	}
+}
---
+++ Compiladores/src/compilador/interpret/Interpreter.java
@@ -0,0 +1,17 @@
+package compilador.interpret;
+
+
+import compilador.node.*;
+
+public class Interpreter {
+	
+/*	public void caseTElse(TElse node) { 
+	       System.out.println(node);
+	       
+	   } 
+	public void caseTEnd(TEnd node) { 
+	       System.out.println(node);
+	       
+	   }*/
+
+}
